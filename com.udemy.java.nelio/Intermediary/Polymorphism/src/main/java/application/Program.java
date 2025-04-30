e6162406146b75aab448e914d7a1be247f4c71d2
diegofregolente <diego_fregolente@hotmail.com>
2025-04-30 12:25:08 -0300

    update

--- com.udemy.java.nelio/Intermediary/Polymorphism/src/main/java/application/Program.java
+++ com.udemy.java.nelio/Intermediary/Polymorphism/src/main/java/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
@@ -9,6 +8,12 @@ public class Program {
 
         Account x = new Account(1001, "Diego", 1000D);
         Account y = new SavingsAccount (1002, "Amanda", 1000D, 0.03);
+
+        x.withDraw(50.0);
+        y.withDraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
 
     }
 }
